a5c555603cc0428c586701a73ceecf36c14b1004
Niema Moshiri <niemamoshiri@gmail.com>
2021-06-27 17:21:36 -0700

    Throw error for BA model (incorrect)

--- barabasi_albert.cpp
+++ barabasi_albert.cpp
@@ -2,6 +2,7 @@
 #include "common.h"
 
 void generate_ba_graph(unsigned long long const & N, unsigned long long const & M) {
+    error("The generate_ba_graph() function has a bug and is incorrect");
     writer.write_nodes(N);
     for(unsigned long long new_node = M; new_node < N; ++new_node) {
         for(unsigned long long target : sample_range_no_replacement(0, new_node-1, M)) {
