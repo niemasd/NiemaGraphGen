7f8e2db4ff828f681f03bc08df1f1945ed6e8557
Niema Moshiri <niemamoshiri@gmail.com>
2021-06-29 09:17:26 -0700

    Removed unnecessary commented-out code

--- ring_lattice.cpp
+++ ring_lattice.cpp
@@ -9,14 +9,3 @@ void generate_ring_lattice_graph(NGG_UINT const & N, NGG_UINT const & K) {
         }
     }
 }
-/*
-void generate_complete_graph(NGG_UINT const & N) {
-    NGG_UINT const N_MINUS_1 = N-1;
-    writer.write_nodes(N);
-    for(NGG_UINT i = 0; i < N_MINUS_1; ++i) {
-        for(NGG_UINT j = i+1; j < N; ++j) {
-            writer.write_edge(i, j);
-        }
-    }
-}
-*/
